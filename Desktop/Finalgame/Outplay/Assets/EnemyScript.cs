6a2b769277d5163a52cf9a4ceb99946bbf1e1e3f
alisedarta <67550554+alisedarta@users.noreply.github.com>
2022-06-06 10:36:39 +0100

    changes

---
+++ Desktop/Finalgame/Outplay/Assets/EnemyScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] GameObject mainPlayer;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] GameObject particles;
+    [SerializeField] AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void spawn()
+    {
+        float posX;
+        float posY;
+        Vector2 pos;
+        for (int i = 0; i < 100; i++)
+        {
+            posX = Random.Range(spriteRenderer.sprite.bounds.min.x - 3.5f, spriteRenderer.sprite.bounds.max.x + 3.5f);
+            posY = Random.Range(spriteRenderer.sprite.bounds.min.y - 1.5f, spriteRenderer.sprite.bounds.max.y + 1.5f);
+            pos = new Vector2(posX, posY);
+
+            Instantiate(prefab, pos, Quaternion.identity);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Enemy")
+        {
+            particles.transform.position = mainPlayer.transform.position;
+            particles.GetComponent<ParticleSystem>().Play();
+           
+            mainPlayer.SetActive(false);
+            audioSource.Play();
+
+        }
+    }
+}
---
+++ Desktop/Finalgame/Outplay/Assets/Movement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] Transform[] points;
+    [SerializeField] float speed;
+    [SerializeField] GameObject particles;
+    [SerializeField] GameObject mainPlayer;
+    [SerializeField] AudioSource audioSource;
+    Transform nextPoint;
+    int nextIndex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextPoint = points[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+
+    void Move()
+    {
+        if(transform.position == nextPoint.position)
+        {
+            nextIndex++;
+            if (nextIndex < points.Length)
+            {
+                nextPoint = points[nextIndex];
+            }
+            
+        }
+        if (transform.position == points[points.Length - 1].position)
+        {
+            particles.transform.position = mainPlayer.transform.position;
+            particles.GetComponent<ParticleSystem>().Play();
+            mainPlayer.SetActive(false);
+            audioSource.Play();
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, nextPoint.position, speed * Time.deltaTime);
+        }
+    }
+}
